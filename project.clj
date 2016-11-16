64e2408afa26544c05976be15aa75eb3366eb453
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-16 21:53:55 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alumbra/generators "0.1.1-SNAPSHOT"
+(defproject alumbra/generators "0.1.1"
   :description "GraphQL Generators for Clojure's test.check"
   :url "https://github.com/alumbra/alumbra.generators"
   :license {:name "MIT License"
